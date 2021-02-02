b8218b48ddaef72b0ff1d0fe0d81db21ece73a41
chirag631 <05042000cdk@gmail.com>
2021-02-02 17:27:22 +0530

    ghcghc

--- app.js
+++ app.js
@@ -39,11 +39,7 @@ app.use('/api/v1',test);
 
 app.use('/users', usersRouter);
 app.use('/testapi',testapirouter);
-app.get('*',(req,res)=>{
-  res.status(200).json({
-    msg:'Catch All'
-  });
-});
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
